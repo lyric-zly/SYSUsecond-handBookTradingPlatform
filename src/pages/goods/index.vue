0863c0c441101c77e480dd3375893b1950c7d71b
Suenaa <1165153863@qq.com>
2018-06-12 16:08:19 +0800

    添加profile和goods，已修正eslint错误，连接服务器和跳转还有问题

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -1,11 +1,181 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div>
+      <list class="list">
+        <cell class="cell" v-for="item in lists" :key="item.tradeId">
+          <div class="panel">
+            <div class="img">
+              <image class="covers" src="https://upload-images.jianshu.io/upload_images/12650180-dd05a051754ddb19.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/400"></image>
+            </div>
+            <div class="info">
+              <div>
+                <text calss="text-h2">{{item.name}}</text>
+                <text class="text-h1">{{item.price}}</text>
+              </div>
+              <div class="op">
+                <text class="buttons">确认收货</text>
+                <text class="buttons">取消交易</text>
+              </div>
+            </div>
+          </div>
+        </cell>
+      </list>
+    </div>
+  </div>
 </template>
 
 <script>
+const stream = weex.requireModule('stream');
+// const modal = weex.requireModule('modal');
 export default {
+  data() {
+    return {
+      lists: [
+        {
+          tradeId: '1',
+          buyerId: '15331059',
+          sellerId: '15331060',
+          bookId: '2',
+          publisherId: '15331060',
+          name: 'test book2',
+          author: 'tinglideng',
+          img: 'default.jpg',
+          campus: 'sysu',
+          state: 3,
+          description: 'test',
+          phoneNum: '13719177172',
+          comment: '计算机 测试',
+          price: 13.7,
+        }, {
+          tradeId: '2',
+          buyerId: '15331059',
+          sellerId: '15331060',
+          bookId: '2',
+          publisherId: '15331060',
+          name: 'test book2',
+          author: 'tinglideng',
+          img: 'default.jpg',
+          campus: 'sysu',
+          state: 3,
+          description: 'test',
+          phoneNum: '13719177172',
+          comment: '计算机 测试',
+          price: 13.7,
+        }],
+    };
+  },
+  created() {
+    const TRADE_URL = 'http://123.207.86.98:3000/api/trade';
+    stream.fetch(
+      {
+        method: 'GET',
+        url: TRADE_URL,
+        type: 'json',
+      },
+      (ret) => {
+        if (!ret.ok) {
+          // console.log('error');
+        } else {
+          // console.log('get' + ret)
+          // this.lists = ret.data;
+        }
+      },
+    );
+  },
+
+  methods: {
+
+  },
 };
 </script>
 
 <style scoped>
+  .container {
+    background-color: #F6F6F6;
+    width: 100%;
+  }
+
+  .img {
+    align-items: center;
+  }
+
+  .covers {
+    height: 300px;
+    width: 200px;
+    margin: 25px;
+  }
+
+  .list {
+    width: 100%;
+  }
+
+  .cell {
+  }
+
+  .panel {
+    height: 350px;
+    background-color: #FFFFFF;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    margin-top: 20px;
+    flex-direction: row;
+  }
+
+  .info {
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 10px;
+    margin-top: 20px;
+    justify-content: space-between;
+  }
+
+  .op {
+    flex-direction: row;
+  }
+
+  .right {
+    width: 46px;
+    height: 46px;
+  }
+
+  .block {
+    background-color: #FFFFFF;
+    width: 100px;
+    top: -35px;
+    margin-left: 200px;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .line {
+    background-color: #585858;
+    height: 0.5px;
+    width: 80px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .buttons {
+    width: 120px;
+    height: 50px;
+    font-size: 26px;
+    border-color: #d4d4d4;
+    border-width: 2px;
+    border-radius: 35%;
+    margin: 10px;
+    padding: 5px;
+  }
+
+  .text-h1 {
+    color: #ECAA96;
+    font-size: 32px;
+    margin-top: 20px;
+  }
+
+  .text-h2 {
+    margin-top: 40px;
+    margin-left: 10px;
+
+  }
 </style>
--- src/pages/home/profile/index.vue
+++ src/pages/home/profile/index.vue
@@ -1,11 +1,192 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div>
+      <div class="userinfo">
+        <text class="text-h5">姓名： {{nickname}}</text>
+        <text class="text-h1">学号： {{studentID}}</text>
+        <text class="text-h2">学院： {{college}}</text>
+      </div>
+      <div class="avatar-container">
+        <image class="userinfo-avatar" src="https://upload-images.jianshu.io/upload_images/12650180-6b048ad0d9bd34aa.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/700" resize="cover"></image>
+      </div>
+    </div>
+    <div class="cells" @click="wantsell">
+      <text class="text-h3">我要卖书</text>
+      <text class="text-h3">></text>
+    </div>
+    <div class="cells">
+      <div class="head">
+        <div class="block">
+          <text class="text-h3">我是买家</text>
+        </div>
+        <div class="line"></div>
+      </div>
+      <div class="item" @click="goTo">
+        <image style="width:96px;height:96px;" src="https://upload-images.jianshu.io/upload_images/12650180-4ffaac6d7e7974f1.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/96"></image>
+        <text class="text-h4">等待收货</text>
+      </div>
+      <div class="item" @click="goTo">
+        <image style="width:96px;height:96px;" src="https://upload-images.jianshu.io/upload_images/12650180-9b967727a94b81ce.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/96"></image>
+        <text class="text-h4">已买入</text>
+      </div>
+    </div>
+    <div class="cells">
+      <div class="head">
+        <div class="block">
+          <text class="text-h3">我是买家</text>
+        </div>
+        <div class="line"></div>
+      </div>
+      <div class="item" @click="goTo">
+        <image style="width:96px;height:96px;" src="https://upload-images.jianshu.io/upload_images/12650180-254508d4005ef76a.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/96"></image>
+        <text class="text-h4">正在出售</text>
+      </div>
+      <div class="item" @click="goTo">
+        <image style="width:96px;height:96px;" src="https://upload-images.jianshu.io/upload_images/12650180-b36db1a96ee2edae.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/96"></image>
+        <text class="text-h4">等待确认</text>
+      </div>
+      <div class="item" @click="goTo">
+        <image style="width:96px;height:96px;" src="https://upload-images.jianshu.io/upload_images/12650180-7f2b966fd2463e48.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/96"></image>
+        <text class="text-h4">已卖出</text>
+      </div>
+    </div>
+    <div class="cells" @click="logout">
+      <text class="text-h3">退出当前帐号</text>
+      <text class="text-h3">></text>
+    </div>
+  </div>
 </template>
 
 <script>
+const stream = weex.requireModule('stream');
+const modal = weex.requireModule('modal');
 export default {
+  data() {
+    return {
+      studentID: '111',
+      nickname: '222',
+      college: '数据科学与计算机学院',
+    };
+  },
+
+  created() {
+    const USER_URL = 'http://123.207.86.98:3000/api/user/login';
+    stream.fetch(
+      {
+        method: 'GET',
+        url: USER_URL,
+        type: 'json',
+      },
+      (ret) => {
+        if (!ret.ok) {
+          const msg = '尚未登录';
+          modal.toast({
+            message: msg,
+            duration: 3.0,
+          });
+        } else {
+          this.studentID = ret.data.studentID;
+          this.nickname = ret.data.nickname;
+        }
+      },
+    );
+  },
+  methods: {
+    goTo() {
+      // this.$router
+      // this.$router.push({ path: '/goods' });
+    },
+    wantsell() {
+
+    },
+    logout() {
+
+    },
+  },
 };
 </script>
 
 <style scoped>
+  .container {
+    background-color: #F6F6F6;
+  }
+
+  .avatar-container {
+    top: -180px;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .userinfo-avatar {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    align-items: center;
+  }
+
+  .userinfo {
+    background-color: #FFFFFF;
+    border-radius: 5%;
+    top: 150px;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+
+  .text-h1 {
+    margin-left: 30px;
+    margin-bottom: 10px;
+  }
+
+  .text-h2 {
+    margin-left: 30px;
+    margin-bottom: 30px;
+  }
+
+  .text-h3 {
+    margin: 30px;
+  }
+
+  .text-h4 {
+    font-size: 26px;
+    color: #7F7F7F;
+  }
+
+  .text-h5 {
+    margin-left: 30px;
+    margin-bottom: 10px;
+    margin-top: 50px;
+  }
+
+  .cells {
+    background-color: #FFFFFF;
+    border-radius: 5%;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .head {
+    flex-direction: row;
+  }
+
+  .block {
+    justify-content: center;
+  }
+
+  .line {
+    width: 0.5px;
+    background-color: #585858;
+    height: 140px;
+    margin: 20px;
+  }
+
+  .item {
+    margin-bottom: 30px;
+    margin-top: 30px;
+    margin-left: 30px;
+    margin-right: 40px;
+    align-items: center;
+  }
 </style>
